381a978c6e18141b33b1098238f9ef125bb1eaec
Guido Alpern <guidoalpern@gmail.com>
2017-05-26 12:10:32 -0300

    Front base funcionando -> Correcion #1

--- components/list-ideas.js
+++
@@ -1,9 +0,0 @@
-const Ideas = (props) => (
-  <div>
-    <ul>
-      <li>{props.name}</li>
-    </ul>
-  </div>
-)
-
-export default Ideas
--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,23 @@
 import React, { Component } from 'react'
 import ReactDOM from 'react-dom'
 
+var adj = ['astonishing', 'amazing', 'awesome', 'stunning', 'marvelous', 'spectacular', 'breathtaking', 'worldchanging']
+
 const Lista = (props) => (
   <div>
     <ul>
-      {props.ideas.map(function(idea, index){
-          return <li key={ index }>{idea}</li>;
-      })}
+      { props.ideas.map((idea, index) => <li key={ index }>{ idea }</li>) }
     </ul>
   </div>
 )
 
 class Index extends React.Component {
   constructor(props) {
-    super(props);
-    this.state = {ideas: []};
+    super(props)
+    this.state = {ideas: []}
 
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleChange = this.handleChange.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
   }
 
   handleSubmit(e) {
@@ -26,25 +26,25 @@ class Index extends React.Component {
     this.setState({
       ideas: ideas,
       value: ''
-    });
+    })
     e.preventDefault()
   }
 
-  handleChange(event) {
-  this.setState({value: event.target.value});
-}
-
+  handleChange(e) {
+    this.setState({value: e.target.value})
+  }
 
   render() {
     return (
       <div>
-        <Lista ideas={this.state.ideas} />
-        <form onSubmit={this.handleSubmit}>
-          <input type="text" name="idea" value={this.state.value} onChange={this.handleChange}/>
+        <h1>The Bucket</h1>
+        <Lista ideas={ this.state.ideas } />
+        <form onSubmit={ this.handleSubmit }>
+          <input type="text" name="idea" value={ this.state.value } placeholder={ "Share your "+adj[Math.floor(Math.random()*adj.length)]+" ideas!" } onChange={ this.handleChange }/>
           <button type="submit">Enviar</button>
         </form>
       </div>
-    );
+    )
   }
 }
 
