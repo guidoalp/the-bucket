b08ba92e2cf05c070dfc622c37645abf141d787f
Guido Alpern <guidoalpern@gmail.com>
2017-06-15 00:52:39 -0300

    Funcion formatDate implementada

--- components/list-ideas.js
+++ components/list-ideas.js
@@ -1,8 +1,12 @@
 import moment from 'moment'
 
+function formatDate(date) {
+  return moment(date).format('DD/MM/YYYY - HH:mm:ss')
+}
+
 const Lista = props =>
   <ul>
-    { props.ideas.map((idea, index) => <li key={ index }>{ moment(idea.date).format('DD/MM/YYYY - HH:mm:ss') } => { idea.content }</li>) }
+    { props.ideas.map((idea, index) => <li key={ index }>{ formatDate(idea.date) } => { idea.content }</li>) }
   </ul>
 
 export default Lista
